304955a1497974c27f3659f8ecc4877cbcccaa61
Achim Friedland <achim.friedland@graphdefined.com>
2024-10-29 18:58:17 +0100

    Added CLI test command 'reset' for OCPP v1.6/v2.1

---
+++ OCPPCSMSTest/CLI/CLICommands/UseCommand.cs
@@ -0,0 +1,163 @@
+﻿/*
+ * Copyright (c) 2014-2024 GraphDefined GmbH <achim.friedland@graphdefined.com>
+ * This file is part of WWCP OCPP <https://github.com/OpenChargingCloud/WWCP_OCPP>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#region Usings
+
+using org.GraphDefined.Vanaheimr.CLI;
+using org.GraphDefined.Vanaheimr.Illias;
+
+using cloud.charging.open.protocols.WWCP.NetworkingNode;
+
+#endregion
+
+namespace org.GraphDefined.OCPP.CSMS.TestApp.CommandLine
+{
+
+    /// <summary>
+    /// Use the networking node with the specified name
+    /// </summary>
+    /// <param name="CLI">The command line interface</param>
+    public class UseCommand(CSMSTestCLI CLI) : ACLICommand<CSMSTestCLI>(CLI),
+                                               ICLICommand
+    {
+
+        #region Data
+
+        public static readonly String CommandName = nameof(UseCommand)[..^7].ToLowerFirstChar();
+
+        #endregion
+
+        #region Suggest(Arguments)
+
+        public override IEnumerable<SuggestionResponse> Suggest(String[] Arguments)
+        {
+
+            if (Arguments.Length == 1)
+            {
+
+                if (CommandName.Equals    (Arguments[0], StringComparison.CurrentCultureIgnoreCase))
+                    return [ SuggestionResponse.CommandCompleted(CommandName) ];
+
+                if (CommandName.StartsWith(Arguments[0], StringComparison.CurrentCultureIgnoreCase))
+                    return [ SuggestionResponse.CommandPrefix   (CommandName) ];
+
+            }
+
+            if (Arguments.Length >= 2 &&
+                CommandName.Equals(Arguments[0], StringComparison.CurrentCultureIgnoreCase))
+            {
+
+                var list = new List<SuggestionResponse>();
+
+                foreach (var connectedNetworkingNodeId in cli.ConnectedNetworkingNodeIds)
+                {
+
+                    var chargingStation = connectedNetworkingNodeId.ToString();
+
+                    if (chargingStation.Equals    (Arguments[1], StringComparison.CurrentCultureIgnoreCase))
+                        list.Add(SuggestionResponse.ParameterCompleted($"{Arguments[0]} {chargingStation}"));
+
+                    if (chargingStation.StartsWith(Arguments[1], StringComparison.CurrentCultureIgnoreCase))
+                        list.Add(SuggestionResponse.ParameterPrefix   ($"{Arguments[0]} {chargingStation}"));
+
+                }
+
+                return list;
+
+            }
+
+            return [];
+
+        }
+
+        #endregion
+
+        #region Execute(Arguments, CancellationToken)
+
+        public override Task<String[]> Execute(String[]           Arguments,
+                                               CancellationToken  CancellationToken)
+        {
+
+            if (Arguments.Length == 2)
+            {
+
+                if (NetworkingNode_Id.TryParse(Arguments[1], out var newRemoteSystemId))
+                {
+
+                    var ocppVersion = "";
+
+                    #region Get the OCPP version for the remote system
+
+                    foreach (var webSocketServer in cli.TestCentralSystemNode.WWCPWebSocketServers)
+                        if (webSocketServer.ConnectedNetworkingNodeIds.Contains(newRemoteSystemId))
+                            ocppVersion = DefaultStrings.OCPPv1_6;
+
+                    foreach (var webSocketServer in cli.TestCSMSNode.WWCPWebSocketServers)
+                        if (webSocketServer.ConnectedNetworkingNodeIds.Contains(newRemoteSystemId))
+                            ocppVersion = DefaultStrings.OCPPv2_1;
+
+                    foreach (var webSocketClient in cli.TestCentralSystemNode.WWCPWebSocketClients)
+                    {
+                        //if (webSocketClient. .Contains(newRemoteSystemId))
+                        //    ocppVersion = DefaultStrings.OCPPv1_6;
+                    }
+
+                    foreach (var webSocketClient in cli.TestCSMSNode.WWCPWebSocketClients)
+                    {
+                        //if (webSocketClient. .Contains(newRemoteSystemId))
+                        //    ocppVersion = DefaultStrings.OCPPv2_1;
+                    }
+
+                    #endregion
+
+                    if (ocppVersion != "")
+                    {
+
+                        if (!cli.Environment.TryAdd(EnvironmentKey.RemoteSystemOCPPVersion, new ConcurrentList<String>(DefaultStrings.OCPPv1_6)))
+                            cli.Environment[EnvironmentKey.RemoteSystemOCPPVersion].TrySet(DefaultStrings.OCPPv1_6);
+
+                        if (!cli.Environment.TryAdd(EnvironmentKey.RemoteSystemId,          new ConcurrentList<String>(newRemoteSystemId.ToString())))
+                            cli.Environment[EnvironmentKey.RemoteSystemId         ].TrySet(newRemoteSystemId.ToString());
+
+                        return Task.FromResult<String[]>([$"Using networking node '{newRemoteSystemId}' ({DefaultStrings.OCPPv1_6})!"]);
+
+                    }
+
+                    return Task.FromResult<String[]>([$"Unknown networking node '{newRemoteSystemId}'!"]);
+
+                }
+
+                return Task.FromResult<String[]>([$"Invalid networking node '{Arguments[1]}'!"]);
+
+            }
+
+            return Task.FromResult<String[]>([$"Usage: {CommandName} <networking node>"]);
+
+        }
+
+        #endregion
+
+        #region Help()
+
+        public override String Help()
+            => $"{CommandName} <networking node> - Use the networking node with the specified name";
+
+        #endregion
+
+    }
+
+}
---
+++ OCPPCSMSTest/CLI/CSMSTestCLI.cs
@@ -0,0 +1,127 @@
+﻿/*
+ * Copyright (c) 2014-2024 GraphDefined GmbH <achim.friedland@graphdefined.com>
+ * This file is part of WWCP OCPP <https://github.com/OpenChargingCloud/WWCP_OCPP>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#region Usings
+
+using System.Reflection;
+using cloud.charging.open.protocols.WWCP.NetworkingNode;
+using org.GraphDefined.Vanaheimr.CLI;
+
+using OCPPv1_6 = cloud.charging.open.protocols.OCPPv1_6;
+using OCPPv2_1 = cloud.charging.open.protocols.OCPPv2_1;
+
+#endregion
+
+namespace org.GraphDefined.OCPP.CSMS.TestApp
+{
+
+    public static class DefaultStrings
+    {
+
+        public const String OCPPv1_6    = "OCPP v1.6";
+        public const String OCPPv2_0_1  = "OCPP v2.0.1";
+        public const String OCPPv2_1    = "OCPP v2.1";
+
+    }
+
+
+    public class CSMSTestCLI : CLI,
+                               OCPPv1_6.CentralSystem.CommandLine.ICentralSystemCLI,
+                               OCPPv2_1.CSMS.CommandLine.ICSMSCLI
+    {
+
+        #region (static class) DefaultStrings
+
+        /// <summary>
+        /// Default strings.
+        /// </summary>
+        public new static class DefaultStrings
+        {
+
+            //public const String ChargingStations = "chargingStations";
+
+        }
+
+        #endregion
+
+
+        #region Properties
+
+        public List<String> ChargingStations { get; } = [];
+
+        public IEnumerable<NetworkingNode_Id> ConnectedNetworkingNodeIds
+        {
+            get
+            {
+
+                var list = new List<NetworkingNode_Id>();
+
+                foreach (var webSocketServer in TestCentralSystemNode.WWCPWebSocketServers)
+                    list.AddRange(webSocketServer.ConnectedNetworkingNodeIds);
+
+                foreach (var webSocketServer in TestCSMSNode.WWCPWebSocketServers)
+                    list.AddRange(webSocketServer.ConnectedNetworkingNodeIds);
+
+                return list.Distinct();
+
+            }
+        }
+
+
+        public OCPPv1_6.TestCentralSystemNode  TestCentralSystemNode    { get; }
+        public OCPPv2_1.CSMS.TestCSMSNode      TestCSMSNode             { get; }
+
+        #region OCPP Adapters
+
+        OCPPv1_6.NetworkingNode.OCPPAdapter OCPPv1_6.CentralSystem.CommandLine.ICentralSystemCLI.OCPP
+            => TestCentralSystemNode.OCPP;
+
+        OCPPv2_1.NetworkingNode.OCPPAdapter OCPPv2_1.CSMS.CommandLine.ICSMSCLI.OCPP
+            => TestCSMSNode.OCPP;
+
+        #endregion
+
+        #endregion
+
+        #region Constructor(s)
+
+        /// <summary>
+        /// Create a new CSMS command line interface.
+        /// </summary>
+        /// <param name="AssembliesWithCLICommands">The assemblies to search for CSMS commands.</param>
+        public CSMSTestCLI(OCPPv1_6.TestCentralSystemNode  TestCentralSystemNode,
+                           OCPPv2_1.CSMS.TestCSMSNode      TestCSMSNode,
+                           params Assembly[]               AssembliesWithCLICommands)
+
+            : base(AssembliesWithCLICommands)
+
+        {
+
+            this.TestCentralSystemNode  = TestCentralSystemNode;
+            this.TestCSMSNode           = TestCSMSNode;
+
+            RegisterCLIType(typeof(CSMSTestCLI));
+            RegisterCLIType(typeof(OCPPv1_6.CentralSystem.CommandLine.ICentralSystemCLI));
+            RegisterCLIType(typeof(OCPPv2_1.CSMS.CommandLine.ICSMSCLI));
+
+        }
+
+        #endregion
+
+    }
+
+}
